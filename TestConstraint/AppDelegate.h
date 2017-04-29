1fb24e331abd5e6bd9987be5173b843515657183
tomfriwel <976932447@qq.com>
2017-04-29 13:13:22 +0800

    Initial Commit

---
+++ TestConstraint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestConstraint
+//
+//  Created by tomfriwel on 29/04/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
